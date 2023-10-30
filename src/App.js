6b23762ced5e9d9f0c695d694b1ef14fe5d3c7f6
Vitalya-Str <Strela2789@gmail.com>
2023-10-30 21:50:40 +0600

    Project-markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from "./Components/Profile/Profile";
+import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Dialogs from "./Components/Dialogs/Dialogs";
+import Users from "./Components/Users/Users";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <header className="App-header">
+            <Header/>
+         </header>
+         <div className="App-sidebar">
+            <Sidebar/>
+         </div>
+
+         <div className="App-content">
+            <Profile/>
+            <Dialogs/>
+            <Users/>
+         </div>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import s from "./Header.module.css"
+
+const Header = () => {
+
+   return (<header className={s.body}>
+      <img  src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" alt="logo"/>
+   </header>)
+}
+
+export default Header
